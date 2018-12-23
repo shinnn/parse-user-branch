60af158c41103f44c3473acd35243c1ea04630d0
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-12-24 08:36:47 +0900

    drop Bower support as it's no longer maintained

--- README.md
+++ README.md
@@ -1,38 +1,28 @@
 # parse-user-branch
 
-[![NPM version](https://img.shields.io/npm/v/parse-user-branch.svg)](https://www.npmjs.com/package/parse-user-branch)
-[![Bower version](https://img.shields.io/bower/v/parse-user-branch.svg)](https://github.com/shinnn/parse-user-branch/releases)
-[![Build Status](https://travis-ci.org/shinnn/parse-user-branch.svg)](https://travis-ci.org/shinnn/parse-user-branch)
+[![npm version](https://img.shields.io/npm/v/parse-user-branch.svg)](https://www.npmjs.com/package/parse-user-branch)
+[![Build Status](https://travis-ci.com/shinnn/parse-user-branch.svg?branch=master)](https://travis-ci.com/shinnn/parse-user-branch)
 [![Coverage Status](https://img.shields.io/coveralls/shinnn/parse-user-branch.svg)](https://coveralls.io/github/shinnn/parse-user-branch?branch=master)
-[![devDependencies Status](https://david-dm.org/shinnn/parse-user-branch/dev-status.svg)](https://david-dm.org/shinnn/parse-user-branch?type=dev)
 
 Parse a `username:branch` string that is available for [Github](https://github.com/) [commit comparison API](https://developer.github.com/v3/repos/commits/#compare-two-commits)
 
 ## Installation
 
-### Package managers
-
-#### [npm](https://www.npmjs.com/)
+[Use](https://docs.npmjs.com/cli/install) [npm](https://docs.npmjs.com/about-npm/).
 
 ```
 npm install parse-user-branch
 ```
 
-#### [Bower](http://bower.io/)
-
-```
-bower install parse-user-branch
-```
-
-### Standalone
-
-[Download the script file directly.](https://raw.githubusercontent.com/shinnn/parse-user-branch/master/browser.js)
-
 ## API
+
+```javascript
+import parseUserBranch from 'parse-user-branch';
+```
 
 ### parseUserBranch(*str*)
 
-*str*: `String` (`username:branch` style branch expression, for example <https://github.com/isaacs/nyc/tree/dep-update> → `'isaacs:dep-update'`)  
+*str*: `string` (`username:branch` style branch expression, for example <https://github.com/isaacs/nyc/tree/dep-update> → `'isaacs:dep-update'`)  
 Return: `Object` (`{username: ..., branch: ...}`)
 
 It extracts a username and a branch name from a `username:branch` string and creates an object.
@@ -44,6 +34,6 @@ parseUserBranch('shinnn:patch-1');
 
 ## License
 
-Copyright (c) 2016 [Shinnosuke Watanabe](https://github.com/shinnn)
+Copyright (c) 2016 - 2018 [Shinnosuke Watanabe](https://github.com/shinnn)
 
 Licensed under [the MIT License](./LICENSE).
--- test.js
+++ test.js
@@ -1,79 +1,64 @@
 'use strict';
 
-const requireFromString = require('require-from-string');
-const rollup = require('rollup');
+const parseUserBranch = require('.');
 const test = require('tape');
 
-function runTest(description, main) {
-  test(description, t => {
-    t.plan(11);
+test('parseUserBranch()', t => {
+  t.deepEqual(parseUserBranch('a:b'), {username: 'a', branch: 'b'}, 'should parse a user:branch string.');
 
-    t.strictEqual(main.name, 'parseUserBranch', 'should have a function name.');
+  t.throws(
+    () => parseUserBranch(),
+    /^TypeError.*undefined is not a string\. Expected a string in the form of "username:branch"\./u,
+    'should throw a type error when it takes no arguments.'
+  );
 
-    t.deepEqual(main('a:b'), {username: 'a', branch: 'b'}, 'should parse a user:branch string.');
+  t.throws(
+    () => parseUserBranch(1),
+    /^TypeError.*1 is not a string\. Expected a string in the form of "username:branch"\./u,
+    'should throw a type error when it takes a non-string argument.'
+  );
 
-    t.throws(
-      () => main(),
-      /^TypeError.*undefined is not a string\. Expected a string in the form of "username:branch"\./,
-      'should throw a type error when it takes no arguments.'
-    );
+  t.throws(
+    () => parseUserBranch(''),
+    /^Error.*Expected a string in the form of "username:repo", but received an empty string\./u,
+    'should throw an error when it takes an empty string.'
+  );
 
-    t.throws(
-      () => main(1),
-      /^TypeError.*1 is not a string\. Expected a string in the form of "username:branch"\./,
-      'should throw a type error when it takes a non-string argument.'
-    );
+  t.throws(
+    () => parseUserBranch('ab'),
+    /^Error.*ab doesn't include `:`\. The string must be in the form of "username:branch"\./u,
+    'should throw an error when the string does not include `:`.'
+  );
 
-    t.throws(
-      () => main(''),
-      /^Error.*Expected a string in the form of "username:repo", but received an empty string\./,
-      'should throw an error when it takes an empty string.'
-    );
+  t.throws(
+    () => parseUserBranch('a::b'),
+    /^Error.*a::b includes more than one colon\. The string must be in the form of "username:branch"\./u,
+    'should throw an error when the string includes multiple `:`.'
+  );
 
-    t.throws(
-      () => main('ab'),
-      /^Error.*ab doesn't include `:`\. The string must be in the form of "username:branch"\./,
-      'should throw an error when the string does not include `:`.'
-    );
+  t.throws(
+    () => parseUserBranch('a: b'),
+    /^Error.*a: b includes a white space\. The string must be in the form of "username:branch"\./u,
+    'should throw an error when the string includes a white space.'
+  );
 
-    t.throws(
-      () => main('a::b'),
-      /^Error.*a::b includes more than one colon\. The string must be in the form of "username:branch"\./,
-      'should throw an error when the string includes multiple `:`.'
-    );
+  t.throws(
+    () => parseUserBranch(':'),
+    /^Error.*The string includes neither username nor branch\. /u,
+    'should throw an error when it takes `:`.'
+  );
 
-    t.throws(
-      () => main('a: b'),
-      /^Error.*a: b includes a white space\. The string must be in the form of "username:branch"\./,
-      'should throw an error when the string includes a white space.'
-    );
+  t.throws(
+    () => parseUserBranch(':b'),
+    /^Error.*:b starts with `:`, in other words no username is specified\. /u,
+    'should throw an error when the string starts with `:`.'
+  );
 
-    t.throws(
-      () => main(':'),
-      /^Error.*The string includes neither username nor branch\. /,
-      'should throw an error when it takes `:`.'
-    );
+  t.throws(
+    () => parseUserBranch('a:'),
+    /Error.*a: ends with `:`, in other words no branch is specified\. /u,
+    'should throw an error when the string ends with `:`.'
+  );
 
-    t.throws(
-      () => main(':b'),
-      /^Error.*:b starts with `:`, in other words no username is specified\. /,
-      'should throw an error when the string starts with `:`.'
-    );
-
-    t.throws(
-      () => main('a:'),
-      /Error.*a: ends with `:`, in other words no branch is specified\. /,
-      'should throw an error when the string ends with `:`.'
-    );
-  });
-}
-
-global.window = {};
-require('./' + require('./bower.json').main);
-
-runTest('require(\'parse-user-branch\')', require('.'));
-runTest('window.parseUserBranch', global.window.parseUserBranch);
-
-rollup.rollup({entry: require('./package.json')['jsnext:main']}).then(bundle => {
-  runTest('Module exports', requireFromString(bundle.generate({format: 'cjs'}).code));
+  t.end();
 });
